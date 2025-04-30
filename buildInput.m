b1df14e9abc335e5e0569b6fc5d8828a91f41d81
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-04-30 16:55:53 +0800

    Update buildInput.m

--- buildInput.m
+++ buildInput.m
@@ -1,29 +1,31 @@
-function I=buildInput(img,map,lambda,n)
+function I=buildInput(img,map,rfRad,lcRad,n)
 
 %根据视觉刺激和方向偏好分布，生成前馈刺激
 %输入：
 %img- 视觉刺激
 %map- 方向偏好图
-%lambda- 感受野尺度参数
+%rfRad- recptive field radius
 %n- V1细胞数量参数（边长）
 
-paddedImg = padarray(img, [lambda lambda], 'replicate');
-rfAlbum=zeros(n,n,2*lambda+1,2*lambda+1);
+paddedImg = padarray(img, [rfRad rfRad], 'replicate');
+rfAlbum=zeros(n,n,2*rfRad+1,2*rfRad+1);
 
 
 
 theta=0;
 for x=1:1:n
     for y=1:1:n
-        theta=180*map(x+4*lambda,y+4*lambda);
-        rfAlbum(x,y,:,:)=generateGabor(lambda, theta);
+        theta=180*map(x+lcRad,y+lcRad);
+        rfAlbum(x,y,:,:)=generateGabor(rfRad, theta);
     end
 end
 
 I=zeros(n);
 for x=1:1:n
     for y=1:1:n
-        neighbor=paddedImg(x*2:x*2+2*lambda,y*2:y*2+2*lambda);
+        neighbor=paddedImg(x*2:x*2+2*rfRad,y*2:y*2+2*rfRad);
         I(x,y)=sum(neighbor.* squeeze(rfAlbum(x,y,:,:)),'all');
     end
+    x=x
 end
+
