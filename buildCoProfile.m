6e68d2838365b0e0b77f10079735052b73a391cd
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-04-30 16:57:50 +0800

    Update buildCoProfile.m

--- buildCoProfile.m
+++ buildCoProfile.m
@@ -1,45 +1,45 @@
-function profileTotal=buildCoProfile(lambda,sigma,map)
+function profileTotal=buildLCProfile(lcRad,lcSigma,map)
 
-profileSE=buildProfileSE(lambda,sigma);
-profileSI=buildProfileSI(lambda,sigma);
-profileLE=buildProfileLE(lambda,sigma,map);
+profileSE=buildProfileSE(lcRad,lcSigma);
+profileSI=buildProfileSI(lcRad,lcSigma);
+profileLE=buildProfileLE(lcRad,lcSigma,map);
 
 profileTotal=(profileSE+profileLE)/2-(profileSI);
 
 %% 短程兴奋性作用
-function profileSE=buildProfileSE(lambda,sigma)
-    n=8*lambda+1;
-    c=4*lambda+1; 
+function profileSE=buildProfileSE(lcRad,lcSigma)
+    n=2*lcRad+1;
+    c=lcRad+1; 
     [X, Y]=meshgrid(1:n); 
     A=sqrt((X-c).^2+(Y-c).^2); 
     A(c,c)=0; 
 
-    profileSE=normpdf(A,0,sigma/4);
+    profileSE=normpdf(A,0,lcSigma/4);
     profileSE=profileSE/sum(profileSE,"all");
 end
 
 
 %% 短程抑制性作用
-function profileSI=buildProfileSI(lambda,sigma)
-    n=8*lambda+1;
-    c=4*lambda+1; 
+function profileSI=buildProfileSI(lcRad,lcSigma)
+    n=2*lcRad+1;
+    c=lcRad+1; 
     [X, Y]=meshgrid(1:n); 
     A=sqrt((X-c).^2+(Y-c).^2); 
     A(c,c)=0; 
 
-    profileSI=normpdf(A,0,sigma/2);
+    profileSI=normpdf(A,0,lcSigma/2);
     profileSI=profileSI/sum(profileSI,"all");
 end
 
 %% 长程兴奋性作用
-function profileLE=buildProfileLE(lambda,sigma,map)
-    n=8*lambda+1;
-    c=4*lambda+1; 
+function profileLE=buildProfileLE(lcRad,lcSigma,map)
+    n=2*lcRad+1;
+    c=lcRad+1; 
     [X, Y]=meshgrid(1:n); 
     A=sqrt((X-c).^2+(Y-c).^2); 
     A(c,c)=0; 
 
-    profileLE=normpdf(A,sigma,sigma/6);
+    profileLE=normpdf(A,lcSigma,lcSigma/6);
 
     clock=abs(map-map(c,c));
     cotClock=abs(map-pi-map(c,c));
@@ -49,6 +49,5 @@ function profileLE=buildProfileLE(lambda,sigma,map)
     profileLE=profileLE/sum(profileLE,"all");
 
 end
-
 
 end
