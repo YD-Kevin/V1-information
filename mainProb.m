822dc47b07d88e17d4534f5ce6eb57e4732b04a9
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-05-06 13:26:24 +0800

    Update mainProb.m

--- mainProb.m
+++ mainProb.m
@@ -7,47 +7,50 @@ lcScale=0.55; %a reference scale factor of lateral connection by mm
 
 %% Parameters
 
-n=256; %256*256 cells, number of cells in this patch of V1 cortex
-m=512; %image input is 256*256 unit pixels
+n=128; %128*128 cells, number of cells in this patch of V1 cortex
+m=256; %image input is 256*256 unit pixels
 nDist=sqrt(1/nd); %(sqrt(n^2/nd)/n), convert the unit distence between cells to mm
 vWindow=n*nDist*mf; %the corespondent vision range of this patch by degree 
 rfRad=round((m/vWindow)*rf/2); %the radius of receptive field by unit pixels
 simFreq=40; %
 lcSigma=lcScale/nDist;
 lcRad=round(lcSigma*1.8);
+a=1;
 
-%% 
-% mapAlbum=zeros(10,lcRad*2+n,lcRad*2+n);
-% 
-% for a=1:1:10
-%     mapAlbum(a,:,:)=buildMap(3/a-0.29,lcRad*2+n);
-% end
-% 
-% actAlbum=zeros(10,40,n,n);
-% 
-% 
-% inputImg = double(imread('cameraman.tif'));%图片输入
-% actAlbum=zeros(10,time,n,n);
 
-% map=buildMap(0.2,lcRad*2+n);
-
-% album=buildAlbum(lcRad,n,lcSigma,map,a);
+%% Test
+ % map=buildMap(0.2,lcRad*2+n);
+ % 
+ % album=buildAlbum(lcRad,n,lcSigma,map,a);
+ % 
+ % img=buildRaster(0,0,15,m);
+ % 
+ % IList=input(rfRad,lcRad,n,m,simFreq,img,map);
+ %
+ % [act,Spk]=evo(lcRad,n,simFreq,album,IList,1);
 
 
 
+mapAlbum=zeros(10,lcRad*2+n,lcRad*2+n);
 
-% img=buildRaster(0,0,15,m);
-% IList=input(rfRad,lcRad,n,m,simFreq,img,map);
+for a=1:1:10
+   mapAlbum(a,:,:)=buildMap(3/a-0.29,lcRad*2+n);
+end
+% album of reference maps with different density.
+
+inputImg = double(imread('cameraman.tif'));
+actAlbum=zeros(10,simFreq,n,n);
+SpkAlbum=zeros(10,simFreq,n,n);
 
 
-
-% for a=1:1:10
-%     map=squeeze(mapAlbum(a,:,:)); %生成偏好图
-%     album=buildAlbum(lambda,n,sigma,map,a);
-%     IList=input(lambda,n,time,inputImg,map);
-%     act=evo(lambda,n,time,album,IList,a);
-%     actAlbum(a,:,:,:)=act;
-% end
+for a=1:1:10
+    map=squeeze(mapAlbum(a,:,:)); 
+    album=buildAlbum(lcRad,n,lcSigma,map,a);
+    IList=input(rfRad,lcRad,n,m,simFreq,inputImg,map);
+    [act,Spk]=evo(lcRad,n,simFreq,album,IList,a);
+    actAlbum(a,:,:,:)=act;
+    SpkAlbum(a,:,:,:)=Spk;
+end
 
 
 
@@ -55,21 +58,20 @@ lcRad=round(lcSigma*1.8);
 
 function album=buildAlbum(lcRad,n,lcSigma,map,a)
 
-    album=zeros(n,n,lcRad*2+1,lcRad*2+1); 
+    album=zeros(n,n,lcRad*2+n,lcRad*2+n); 
     subMap=zeros(lcRad*2+1);
-    
+    count=0;
     %根据偏好图绘制各个神经元对周边神经元的作用分布图
     for x=1:1:n
         for y=1:1:n
     
             subMap=map(x:x+lcRad*2,y:y+lcRad*2);
             subMap=buildLCProfile(lcRad,lcSigma,subMap);
-
-    
-            album(x,y,:,:)=subMap;
+   
+            album(x,y,x:x+2*lcRad,y:y+2*lcRad)=subMap;
             
         end
-        count=x/n
+        count=((a-1)*128+x)/n
     end
 
 end
@@ -113,20 +115,28 @@ end
 
 
 %% 计算神经活动演化
-function [act,actCount]=evo(lambda,n,time,album,IList,a)
-
- act=zeros(time,n,n);
- actCount=zeros(n,n);
+function [act,Spk]=evo(lcRad,n,simFreq,album,IList,a)
+ count=0;
+ act=zeros(simFreq,n,n);
  act(1,:,:)=rand(n);
- for t=2:1:time
+ Spk=zeros(simFreq,n,n);
+ for t=2:1:simFreq
      prevAct=squeeze(act(t-1,:,:));
-     actualSpk=poissrnd(prevAct/time);
-     actCount=actCount+actualSpk;
-     bingo=squeeze(sum(album .* (musk*time), [1 2]));
+     actualSpk=poissrnd(prevAct/simFreq);
+     Spk(t,:,:)=actualSpk;
+     % bingo=zeros(n+2*lcRad);
+     % for x=1:n
+     %     for y=1:n
+     %         bingo(x:x+2*lcRad,y:y+2*lcRad)=bingo(x:x+2*lcRad,y:y+2*lcRad)+(actualSpk(x,y)*simFreq*squeeze(album(x,y,:,:)));
+     %     end
+     %     x=x*count
+     % end
+     bingo=squeeze(sum(album .* (actualSpk*simFreq), [1 2]));
      bingo=bingo(lcRad+1:lcRad+n,lcRad+1:lcRad+n);
      act(t,:,:)=relu(-0.6*prevAct+bingo+squeeze(IList(t,:,:)));
-     count=((a-1)*168+128+t)/1680
+     count=((a-1)*40+t)/40
  end
+
 end
 
 function pos=relu(rate)
