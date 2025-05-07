86ff66c46f04dd89f5bcceae1e41f624272afefc
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-05-07 14:27:41 +0800

    Add files via upload

---
+++ buildColorBlobs_1.m
@@ -0,0 +1,44 @@
+function blobs=buildColorBlobs_1(map,rho,threshold,majorAxis,minorAxis,noiseLevel)
+
+
+Fx=cos(map*2-pi);
+Fy=sin(map*2-pi);
+
+[dfydx, ~]=gradient(Fy); % dFy/dx
+[~, dfxdy]=gradient(Fx); % dFx/dy
+
+curl =dfydx-dfxdy;
+binaryCurl=double(abs(curl) >= threshold);
+
+    
+
+[nRows, nCols]=size(binaryCurl);
+finalMask=false(nRows, nCols);
+
+
+[yCoords, xCoords]=find(binaryCurl);
+
+
+theta=linspace(0, 2*pi, 100); 
+baseR=(majorAxis * minorAxis) ./ sqrt((minorAxis*cos(theta)).^2+(majorAxis*sin(theta)).^2);
+
+
+for i=1:length(xCoords)
+   
+    noise=1 + noiseLevel * randn(size(theta));
+    distortedR=baseR .* noise;
+    
+   
+    x=xCoords(i) + distortedR .* cos(theta);
+    y=yCoords(i) + distortedR .* sin(theta);
+    
+    
+    mask=poly2mask(x, y, nRows, nCols);
+    finalMask=finalMask | mask;
+end
+
+shiftedMap=buildMap(rho,lcRad*2+n,0.5,0.5);
+blobs=shiftedMap .* finalMask;
+
+end
+
