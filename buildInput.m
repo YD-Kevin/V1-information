d229217cdb550a8fc52e78a4f2130d801803364b
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-05-13 15:15:28 +0800

    Update buildInput.m

--- buildInput.m
+++ buildInput.m
@@ -1,4 +1,4 @@
-function I=buildInput(img,map,rfRad,lcRad,n)
+function I=buildInput(img,rfAlbum,rfRad,n)
 
 %根据视觉刺激和方向偏好分布，生成前馈刺激
 %输入：
@@ -8,17 +8,7 @@ function I=buildInput(img,map,rfRad,lcRad,n)
 %n- V1细胞数量参数（边长）
 
 paddedImg = padarray(img, [rfRad rfRad], 'replicate');
-rfAlbum=zeros(n,n,2*rfRad+1,2*rfRad+1);
 
-
-
-theta=0;
-for x=1:1:n
-    for y=1:1:n
-        theta=180*map(x+lcRad,y+lcRad);
-        rfAlbum(x,y,:,:)=generateGabor(rfRad, theta);
-    end
-end
 
 I=zeros(n);
 for x=1:1:n
@@ -27,4 +17,3 @@ for x=1:1:n
         I(x,y)=sum(neighbor.* squeeze(rfAlbum(x,y,:,:)),'all');
     end
 end
-
