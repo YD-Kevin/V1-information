fa742b041c06a635e02eb465fc9c01e40a3d088c
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-05-15 22:26:02 +0800

    Update buildMap.m

--- buildMap.m
+++ buildMap.m
@@ -1,9 +1,11 @@
-function theta_map=buildMap(rho,N)
+function theta_map=buildMap(rho,N,xShift,yShift)
 
     % 生成方向偏好图的二维矩阵
     % 输入:
-    %   rho - 密度参数
-    %   N   - 网格尺寸
+    %   rho    密度参数
+    %    N    网格尺寸
+    %   xShift    x方向移相
+    %   yShift    y方向移相
     
     % 输出:
     %   theta_map - N×N的方向偏好矩阵，元素为0到pi的方向
@@ -18,19 +20,22 @@ function theta_map=buildMap(rho,N)
     Gamma=2*pi*rand(1,K);  % 随机相位[0, 2π)
     
     % 生成坐标网格（0-based）
-    [x1,x2]=meshgrid(0:N-1,0:N-1);
+    xShift=xShift*pi/rho;
+    yShift=yShift*pi/rho;
+    [x,y]=meshgrid(xShift:N-1+xShift,yShift:N-1+yShift);
     
     % 计算复数值场phi
     phi=zeros(N, N, 'like', 1i); % 初始化复数矩阵
     for k=1:K
         c=cos(alpha(k));
         s=sin(alpha(k));
-        phase=rho*(x1*c+x2*s)+Gamma(k); % 相位计算
+        phase=rho*(x*c+y*s)+Gamma(k); % 相位计算
         phi=phi+exp(1i*phase)*d_alpha;    % 积分累加
     end
     
     % 计算方向映射
     angle_phi=angle(phi);           % 获取相位角[-pi, pi]
     theta_map=(angle_phi+pi)/2;       %映射到[0,pi]
-
+    theta_map=floor(theta_map*12/pi);
+    theta_map=theta_map*pi/12;
 end
