4313fe67a61ce4b141e1eb0dda0acab5871f482d
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-07-07 16:44:46 +0800

    Add files via upload

---
+++ Acuity/misrecognitionNRFs.m
@@ -0,0 +1,24 @@
+function misRec=misrecognitionNRFs(n,sample,sigmaRFs,noiseLevel,pos)
+
+
+sigmaNoise=(noiseLevel/2)*(1/(sigmaRFs*sqrt(2*pi)));
+GTresponses=posToResponses(pos,n,sigmaRFs);
+
+sampleResponses=zeros(sample,n);
+for i=1:sample
+    sampleResponses(i,:)=posToResponses((i-1)*(1/(sample-1)),n,sigmaRFs);
+end
+
+extGTresponses=repmat(reshape(GTresponses, [1, n]), [sample,1]);
+
+distance=sum((sampleResponses-extGTresponses).^2,[2]);
+
+misRec=1./exp((distance.^2)/(sigmaNoise^2));
+misRec=sample*misRec./(sum(misRec,"all"));
+
+    function responses=posToResponses(pos,n,sigmaRFs)
+        gap=1/(n+1);
+        peaks=linspace(gap,1-gap,n);
+        responses=normpdf(pos,peaks,sigmaRFs)+3;
+    end
+end
