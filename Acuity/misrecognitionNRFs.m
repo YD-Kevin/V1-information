4c827994d7b364537ddcee246eac72fbe334c63b
YD-Kevin <100449570+YD-Kevin@users.noreply.github.com>
2025-07-23 20:06:05 +0800

    Update misrecognitionNRFs.m

--- Acuity/misrecognitionNRFs.m
+++ Acuity/misrecognitionNRFs.m
@@ -1,25 +1,32 @@
-function misRec=misrecognitionNRFs(n,sample,sigmaRFs,noiseLevel,pos)
+function misRec=misrecognitionNRFs(n,sample,FWHM,noiseLevel,pos)
+
+    sigmaRFs=FWHM/2*sqrt(2*log(2)); 
+    sigmaNoise=(noiseLevel/2)*(1/(sigmaRFs*sqrt(2*pi)));
+    GTresponses=posToResponses(pos,n,sigmaRFs);
+    gVal=zeros(sample,1);
 
 
-sigmaNoise=(noiseLevel/2)*(1/(sigmaRFs*sqrt(2*pi)));
-GTresponses=posToResponses(pos,n,sigmaRFs);
-
-sampleResponses=zeros(sample,n);
-for i=1:sample
-    sampleResponses(i,:)=posToResponses((i-1)*(1/(sample-1)),n,sigmaRFs);
-end
-
-extGTresponses=repmat(reshape(GTresponses, [1, n]), [sample,1]);
-
-distanceSQ=sum((sampleResponses-extGTresponses).^2,[2]);
-
-misRec=(0.5*exp(-distanceSQ/(8*sigmaNoise^2)))./(1-0.5*exp(-distanceSQ/(8*sigmaNoise^2)));
-
-misRec=sample*misRec./(sum(misRec,"all"));
-
-    function responses=posToResponses(pos,n,sigmaRFs)
-        gap=1/(n+1);
-        peaks=linspace(gap,1-gap,n);
-        responses=normpdf(pos,peaks,sigmaRFs);
+    sampleResponses=zeros(sample,n);
+    for i=1:sample
+        temSampleResponses=posToResponses((i-1)*(1/(sample-1)),n,sigmaRFs);
+        l=sqrt(sum((temSampleResponses-GTresponses).^2,"all"));
+        sampleResponses(i,:)=temSampleResponses;
+        integrand = @(r1) (1 ./ (sqrt(2*pi))) .* sech(l.*r1./(2.*sigmaNoise)) .* exp(-r1.^2./2);
+        gVal(i)=integral(integrand, -Inf, Inf);
     end
+    
+    extGTresponses=repmat(reshape(GTresponses, [1, n]), [sample,1]);
+    
+    distanceSQ=sum((sampleResponses-extGTresponses).^2,[2]);
+    
+    B=(0.5*exp(-distanceSQ/(8*sigmaNoise^2))).*gVal;
+    misRec=B./(1-B);
+    
+    misRec=sample*misRec./(sum(misRec,"all"));
+    
+        function responses=posToResponses(pos,n,sigmaRFs)
+            gap=1/(n+1);
+            peaks=linspace(gap,1-gap,n);
+            responses=normpdf(pos,peaks,sigmaRFs);
+        end
 end
